2de398bcf4aa142c7b82247e63f802047ff43016
Florencia Fuentes <fuentesflorenciafatimadelosangeles@iesconcepcion.edu.ar>
2025-02-26 20:55:33 -0300

    condiciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Peticiones } from './Componentes/Peticiones.jsx'
 import './App.css'
+import { Condicion } from './Componentes/Condicion.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
      <Peticiones  />
+     <Condicion edad={18} />
+
     </>
   )
 }
---
+++ src/Componentes/Condicion.jsx
@@ -0,0 +1,12 @@
+
+export const Condicion = ({edad}) => {
+    if (edad <= 18){
+        alert ("Menor de edad")
+    }
+    else {
+        alert ("Mayor de edad")
+    }
+
+return <>
+</>
+}
